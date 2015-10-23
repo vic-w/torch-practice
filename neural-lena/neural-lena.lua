f4a5973c128374f6e5f4a7ff164208066f9bcbe2
vic-w <vic.wu@hotmail.com>
2015-10-23 13:46:07 +0800

    changed a simple picture

--- neural-lena/neural-lena.lua
+++ neural-lena/neural-lena.lua
@@ -1,6 +1,7 @@
 require 'image'
 require 'torch'
 require 'nn'
+require 'qtwidget'
 
 function gradientUpdate(model, x, y, learningRate)
    local criterion = nn.MSECriterion()
@@ -13,17 +14,20 @@ function gradientUpdate(model, x, y, learningRate)
    return err
 end
 
-l=image.lena()
-image.window()
-image.display(l)
+--l=image.lena()
+l=image.load('test.jpg')
+l=l[{{1},{},{}}]
+window=qtwidget.newwindow(512,512)
+image.display({image=l,win=window})
  
 model = nn.Sequential()
-layer1 = nn.Linear(2,20)
+layer1 = nn.Linear(2,10)
 model:add(layer1)
 model:add(nn.Sigmoid())
---model:add(nn.Linear(20,20))
+layer2 = nn.Linear(10,1)
+model:add(layer2)
 --model:add(nn.Sigmoid())
-model:add(nn.Linear(20,3))
+--model:add(nn.Linear(20,3))
 
 inputs={}
 for y=1,512 do
@@ -32,15 +36,20 @@ for y=1,512 do
  end
 end
 inputs = torch.Tensor(inputs)
-targets = l:reshape(3,512*512):t()
+targets = l:reshape(1,512*512):t()
 
 
-for n=1,100 do
-for n2=1,10 do
- err = gradientUpdate(model, inputs, targets, 0.01)
+for n=1,10000 do
+for n2=1,100 do
+ err = gradientUpdate(model, inputs, targets, 0.8)
 end
- print(err)
- output = model:forward(inputs):t():reshape(3,512,512)
- image.display(output)
+ collectgarbage()
+ --print('layer1 w', layer1.weight)
+ --print('layer1 b', layer1.bias)
+ --print('layer2 w', layer2.weight)
+ --print('layer2 b', layer2.bias)
+ print('err', err)
+ output = model:forward(inputs):t():reshape(1,512,512)
+ image.display({image=output,win=window})
 end
 
